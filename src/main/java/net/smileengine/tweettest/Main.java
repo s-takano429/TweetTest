a49b3a206a9471bc5c7aaa7a17959d831cfe0948
Shinya Takano <supersonic429@gmail.com>
2014-08-21 12:46:07 +0900

    ソース修正

--- src/main/java/net/smileengine/tweettest/Main.java
+++ src/main/java/net/smileengine/tweettest/Main.java
@@ -10,17 +10,12 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
 
 public class Main 
 {
     public static void main( String[] args ) throws TwitterException
     {
         Twitter twitter = new TwitterFactory().getInstance();
-        User user = twitter.verifyCredentials();
-        
-      
-        //ついーとしてみる
         Status status = twitter.updateStatus("Jenkinsデプロイテスト");
     }
 }
